3fdff137fa753a55d76e8add288c2bcdb7ae36a0
Marco <gdmarav374@gmail.com>
2022-01-31 20:32:26 +0100

    Improve naming

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,7 +2,7 @@
 
 class Goal < ApplicationRecord
   validates :title, presence: true, length: { maximum: 180 }, allow_blank: false
-  validate :end_date_is_after_start_date
+  validate :end_date_is_after_start_date?
 
   has_many :key_results
   belongs_to :owner, class_name: "User"
@@ -19,7 +19,7 @@ class Goal < ApplicationRecord
 
   private
 
-  def end_date_is_after_start_date
+  def end_date_is_after_start_date?
     return if end_date.blank? || start_date.blank?
 
     errors.add(:end_date, "can't be before to start_date") if end_date < start_date
