e62632085bd44b7a86979e4dedead6e7fa0d494c
Marco <gdmarav374@gmail.com>
2022-02-06 22:03:00 +0100

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,6 @@ user = User.create(username: "cruella", password: "letmein1")
 Goal.create(title: "Run 21k in Stockholm", owner: user)
 Goal.create(title: "Visit Japan", owner: user)
 
-completed_goal = Goal.create(title: "Learn Swedish", owner: user, start_date: 1.month.ago, end_date: 1.day.ago)
+completed_goal = Goal.create(title: "Learn Swedish", owner: user, started_at: 1.month.ago, ended_at: 1.day.ago)
 
 FactoryBot.create(:key_result, :completed, goal: completed_goal)
