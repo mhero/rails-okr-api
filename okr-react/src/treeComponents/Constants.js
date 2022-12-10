b8eda6257a167592acc2fd5a3e700d3a82af8f3b
Marco <gdmarav374@gmail.com>
2022-12-10 14:54:00 -0500

    Remove unnecessary logic

--- okr-react/src/treeComponents/Constants.js
+++ okr-react/src/treeComponents/Constants.js
@@ -1,12 +1,14 @@
-import initialTree from "./InitialTree";
-
 export const FOLDER_ID = "folder";
 export const CHILDREN_ID = "children";
 
 export const initialState = {
   active: null,
   tree: {
-    ...initialTree,
+    title: "OKR tree",
+    id: "root-0",
+    children: [],
+    collapsed: false,
   },
   collapsed: false, // start with unmodified tree
 };
+
--- okr-react/src/treeComponents/InitialTree.js
+++
@@ -1,6 +0,0 @@
-export default {
-  title: "OKR tree",
-  id: "root-0",
-  children: [],
-  collapsed: false,
-};
