f5271237d8481f19b7f3a9b060e408f188821d34
Marco <gdmarav374@gmail.com>
2023-07-20 05:12:25 -0500

    Improve rename method

--- okr-react/src/request/Middleware.js
+++ okr-react/src/request/Middleware.js
@@ -1,10 +1,25 @@
 import axios from 'axios';
 
+const renameKeys = (obj, oldKey, newKey) => {
+  if (obj === null || typeof obj !== 'object') {
+    return obj;
+  }
+
+  return Array.isArray(obj)
+    ? obj.map(item => renameKeys(item, oldKey, newKey))
+    : Object.fromEntries(
+      Object.entries(obj).map(([key, value]) => [
+        key === oldKey ? newKey : key,
+        renameKeys(value, oldKey, newKey)
+      ])
+    );
+}
+
 const postOkrTree = async (tree, owner_id) => {
   tree.goals = JSON.parse(JSON.stringify(tree.children));
   tree.owner_id = owner_id;
 
-  const newTree = JSON.stringify(tree).replace(/"children":/g, '"key_results_attributes":');
+  const newTree = renameKeys(tree, 'children', 'key_results_attributes');
 
   try {
     const response = await axios
